c617a13ed4138f32ee79a653d3a91191cc60d105
zeyu <zeyu@gritworld.com>
2025-12-22 10:15:01 +0800

    config NeoVide

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  vim.g.neovide_position_animation_length = 0.03
+  vim.g.neovide_scroll_animation_length = 0.3
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+end
