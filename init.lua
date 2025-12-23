7228be2440be14c70e19b6a650f78c0d502e4168
zeyu <zeyu@gritworld.com>
2025-12-23 16:02:43 +0800

    speed up animation

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ require("config.lazy")
 
 if vim.g.neovide then
   vim.g.neovide_position_animation_length = 0.03
-  vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_scroll_animation_length = 0.1
   vim.o.guifont = "JetBrainsMono Nerd Font:h12"
 end
