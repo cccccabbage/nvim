2f511820699a1077bbe83d9f38b0c14a9516f966
cccccabbage <cccccabbage@gmail.com>
2026-04-29 11:10:11 +0800

    refactor(keymaps): reorganize buffer/window bindings and add keymap docs

---
+++ keymap.md
@@ -0,0 +1,47 @@
+# Keymaps
+
+Leader key: `<Space>`
+
+## General
+
+| Shortcut | Description |
+|----------|-------------|
+| `<Esc>` | Clear search highlights |
+| `<leader>q` | Quit window |
+
+## Buffers
+
+| Shortcut | Description |
+|----------|-------------|
+| `<leader>bc` | Close current buffer |
+| `<leader>bo` | Close other buffers |
+| `<C-H>` | Previous buffer |
+| `<C-L>` | Next buffer |
+
+## Windows
+
+| Shortcut | Description |
+|----------|-------------|
+| `<C-j>` | Move to lower window |
+| `<C-k>` | Move to upper window |
+| `<leader>wh` | Split window left |
+| `<leader>wj` | Split window down |
+| `<leader>wk` | Split window up |
+| `<leader>wl` | Split window right |
+
+## Telescope / Search
+
+| Shortcut | Description |
+|----------|-------------|
+| `<leader>fp` | Find project |
+| `<leader>ft` | Find TODOs |
+| `g/` | Global search (regex, from git root) |
+
+## Tools
+
+| Shortcut | Description |
+|----------|-------------|
+| `<leader>tf` | Toggle format on save |
+| `<leader>gg` | LazyGit |
+| `<leader>e` | Yazi (cwd) |
+| `<leader>E` | Yazi (project root) |
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,15 +2,27 @@ local keymap = vim.keymap.set
 
 keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-keymap("n", "<leader>w", "<cmd>write<CR>", { desc = "Write file" })
 keymap("n", "<leader>q", "<cmd>quit<CR>", { desc = "Quit window" })
 
-keymap("n", "<C-w>", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
+keymap("n", "<leader>bc", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
+keymap("n", "<leader>bo", function()
+  local cur = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buflisted and buf ~= cur then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, { desc = "Close other buffers" })
 keymap("n", "<C-H>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 keymap("n", "<C-L>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 
 keymap("n", "<C-j>", "<C-w><C-j>", { desc = "Move to lower window" })
 keymap("n", "<C-k>", "<C-w><C-k>", { desc = "Move to upper window" })
+
+keymap("n", "<leader>wh", "<cmd>leftabove vsplit<CR>", { desc = "Split window left" })
+keymap("n", "<leader>wj", "<cmd>rightbelow split<CR>", { desc = "Split window down" })
+keymap("n", "<leader>wk", "<cmd>leftabove split<CR>", { desc = "Split window up" })
+keymap("n", "<leader>wl", "<cmd>rightbelow vsplit<CR>", { desc = "Split window right" })
 
 keymap("n", "<leader>fp", "<cmd>Telescope projects<CR>", { desc = "Find project" })
 keymap("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find TODOs" })
